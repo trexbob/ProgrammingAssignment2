16902745adc29a9a311a8f30eef29613ebcd1e95
Robert <robtyx@yahoo.com>
2014-09-18 18:41:44 -0400

    Changes to try to fix code
    Trying here to actually make working code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
 ## These functions create an inverted matrix
-## and solve
+## and solve the inversion of the matrix if it is not already cached
 
-## Creates special matrix object that can cache its invert
+## Creates special matrix object 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrixA <- function(matrixA) m <<- matrixA
+  getmatrixA <- function() m
+  list(set = set, get = get,
+       setmatrixA = setmatrixA,
+       getmatrixA = getmatrixA)
 }
 
 
-## inverts the matrix
+## inverts the matrix based on whether it is cached or not
 
 cacheSolve <- function(x, ...) {
-        solve(x)
+  m <- x$getmatrixA()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(x)
+  m      
 }
